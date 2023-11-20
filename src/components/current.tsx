b68dbcce4a1d281859deb4fa96474d4beb4a5715
BsoftLimited <bsoftlimited@gmail.com>
2023-11-20 03:59:30 +0100

    did some advance responsive shit

--- src/components/current.tsx
+++ src/components/current.tsx
@@ -9,14 +9,14 @@ const CurrentView = () =>{
 
     return (
         <div className="weatherDetails">
-            <div className="location">{location?.place}<span style={{ fontSize: 16 }}> - { forcast?.timezone }</span></div>
-            <div style={{ display:"flex", marginBottom: -30, padding:0, alignItems:"center" }}>
-                <h2 className="temperature">{ Math.round(forcast!.current.temp) }<span style={{ fontSize: 50, lineHeight:"auto" }}>{ "℃" }</span></h2>
+            <div className="location">{location?.place}<span> - { forcast?.timezone }</span></div>
+            <div style={{ display:"flex", marginBottom: "-1.3rem", padding:0, alignItems:"center" }}>
+                <h2 className="temperature">{ Math.round(forcast!.current.temp) }<span className="degree">{ "℃" }</span></h2>
                 <img src={`https://openweathermap.org/img/wn/${forcast!.current.weather[0].icon}@4x.png`} alt="" id="image" />
             </div>
             <div className="description">
-                <p style={{ fontSize: 22, fontWeight: 300, }} className="weatherReport">{ forcast!.current.weather[0].description }</p>
-                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", fontSize: 22, fontWeight: 200, marginTop: 10, width: 200, color: "white" }}>
+                <p className="weatherReport">{ forcast!.current.weather[0].description }</p>
+                <div className="wetherReport-others">
                     <div style={{ display: "flex", alignItems: "center", }}><BiDroplet /> { forcast?.current.humidity }</div>
                     <div style={{ display: "flex", alignItems: "center", }}><BiWind /> { forcast?.current.wind_speed }</div>
                 </div>
